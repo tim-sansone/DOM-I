97c2ba2c5d455a156cd2753103fd2b2bdcd87e85
Tim Sansone <tim.sansone@Tim-Sansone.local>
2022-05-09 16:23:23 -0500

    text and classes complete

--- src/index.js
+++ src/index.js
@@ -40,3 +40,40 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
 };
 
 console.log('project wired!')
+
+const headerNav = document.querySelectorAll("nav a");
+headerNav[0].textContent = siteContent["nav"]["nav-item-1"];
+headerNav[1].textContent = siteContent["nav"]["nav-item-2"];
+headerNav[2].textContent = siteContent["nav"]["nav-item-3"];
+headerNav[3].textContent = siteContent["nav"]["nav-item-4"];
+headerNav[4].textContent = siteContent["nav"]["nav-item-5"];
+headerNav[5].textContent = siteContent["nav"]["nav-item-6"];
+
+headerNav.forEach(each => each.className = "italic");
+
+document.querySelector('.cta-text h1').textContent = siteContent.cta.h1;
+document.querySelector(".cta-text button").textContent = siteContent.cta.button;
+
+
+document.querySelector(".top-content .text-content:nth-of-type(1) h4").textContent = siteContent["main-content"]["features-h4"];
+document.querySelector(".top-content .text-content:nth-of-type(1) p").textContent = siteContent["main-content"]["features-content"];
+
+document.querySelector(".top-content .text-content:nth-of-type(2) h4").textContent = siteContent["main-content"]["about-h4"];
+document.querySelector(".top-content .text-content:nth-of-type(2) p").textContent = siteContent["main-content"]["about-content"];
+
+document.querySelector(".bottom-content .text-content:nth-of-type(1) h4").textContent = siteContent["main-content"]["services-h4"];
+document.querySelector(".bottom-content .text-content:nth-of-type(1) p").textContent = siteContent["main-content"]["services-content"];
+
+document.querySelector(".bottom-content .text-content:nth-of-type(2) h4").textContent = siteContent["main-content"]["product-h4"];
+document.querySelector(".bottom-content .text-content:nth-of-type(2) p").textContent = siteContent["main-content"]["product-content"];
+
+document.querySelector(".bottom-content .text-content:nth-of-type(3) h4").textContent = siteContent["main-content"]["vision-h4"];
+document.querySelector(".bottom-content .text-content:nth-of-type(3) p").textContent = siteContent["main-content"]["vision-content"];
+
+document.querySelector(".contact h4").textContent = siteContent.contact["contact-h4"];
+document.querySelector(".contact p:nth-of-type(1)").textContent = siteContent.contact.address;
+document.querySelector(".contact p:nth-of-type(2)").textContent = siteContent.contact.phone;
+document.querySelector(".contact p:nth-of-type(3)").textContent = siteContent.contact.email;
+
+document.querySelector("footer a").textContent = siteContent.footer.copyright;
+document.querySelector("footer a").className = "bold";
